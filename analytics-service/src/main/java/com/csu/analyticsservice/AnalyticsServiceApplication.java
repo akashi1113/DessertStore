f12ec1a3387c59aa65c590c2390f742fac141a45
逐数 <693786619@qq.com>
2025-11-22 17:02:51 +0800

    新增分析微服务

---
+++ analytics-service/src/main/java/com/csu/analyticsservice/AnalyticsServiceApplication.java
@@ -0,0 +1,17 @@
+package com.csu.analyticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient    // 启用服务注册发现
+@EnableFeignClients       // 启用Feign客户端
+public class AnalyticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticsServiceApplication.class, args);
+    }
+
+}
